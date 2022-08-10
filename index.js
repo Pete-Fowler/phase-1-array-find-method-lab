41d29d142c3be00de1e4ed27b2c0d5fed111ad98
Pete Fowler <fowler.pete@gmail.com>
2022-08-10 11:45:24 -0600

    commit tree 88de45712f2d6d4731a486036f0d908197b30dd2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(arr) {
+  const game = arr.find(({ result }) => result === 'W');
+  if(game) {
+  return game.year;
+  } else {
+    return undefined;
+  }
+}
